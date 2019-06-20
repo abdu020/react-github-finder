a2055242cb581e0492b61f76aaf45f9895ffd9e9
abdu020 <you@example.com>
2019-06-20 12:40:35 +0200

    Refactored to use functional components

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,27 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class Useritem extends Component {
-  render() {
-    // Destructuring where we pull stuff out from the object
-    const { login, avatar_url, html_url } = this.props.user;
-
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+// Destructuring where we pull stuff out from the object
+const Useritem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Useritem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default Useritem;
