fa4b9ecd57928249fff86036b1fe0a12d9813ea1
abdu020 <you@example.com>
2019-06-21 12:35:53 +0200

    refactored to use functional component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import UserItem from "./Useritem";
+import Spinner from "../layouts/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  // Loading is being passed in from app.js state
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 
 const userStyle = {
   display: "grid",
