58d243c283095876f2e1cb6fbfa355c67e4846ce
abdu020 <you@example.com>
2019-06-22 14:24:10 +0200

    Added a clear button and also a event that clear users from state

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./Useritem";
 import Spinner from "../layouts/Spinner";
 import PropTypes from "prop-types";
