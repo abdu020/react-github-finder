f67ef61049fb274fe2c86b431614094fbc562924
abdu020 <you@example.com>
2019-06-22 15:25:49 +0200

    Imported the search and alert component to app.js and also connected them to the state

--- src/App.js
+++ src/App.js
@@ -2,17 +2,73 @@ import React, { Component } from "react";
 import Navbar from "./components/layouts/Navbar";
 import "./App.css";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layouts/Alert";
+import axios from "axios";
 
 class App extends Component {
-  // this is part of the app, works like the constructor
-  foo = () => "Bars";
+  state = {
+    users: [],
+    loading: false,
+    alert: null
+  };
+  // Using axios with async await
+  /*   async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
+    // reset state
+    this.setState({ users: res.data.items, loading: false });
+  } */
+
+  // Search Github users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    // reset state
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear users from state
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
+  // Using axios with promises
+  /*   componentDidMount() {
+    axios
+      .get("https://api.github.com/users")
+      .then(res => console.log(res.data));
+  } */
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
