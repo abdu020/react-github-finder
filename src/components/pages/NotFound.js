6a44a572a26c24f9fdaa3f67577ac75bd798ed96
abdu020 <you@example.com>
2019-07-25 18:46:45 +0200

    Added a not found page

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className="lead">What you are looking for is not here</p>
+    </div>
+  );
+};
+
+export default NotFound;
